51fd557e0eb7cc87a3d70b038512abbb428512fa
Joao Gabriel <123424925+artesssn@users.noreply.github.com>
2025-11-30 22:27:41 -0300

    Add files via upload

---
+++ 2H1/2H1/Services/CursoService.cs
@@ -0,0 +1,50 @@
+﻿public class CursoService : ICursoService
+{
+    private readonly AppDbContext _context;
+
+    public CursoService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Curso>> GetAll()
+        => await _context.Cursos.ToListAsync();
+
+    public async Task<Curso> GetById(int id)
+        => await _context.Cursos.FindAsync(id);
+
+    public async Task<Curso> Add(CursoDTO dto)
+    {
+        var curso = new Curso
+        {
+            Nome = dto.Nome,
+            CargaHoraria = dto.CargaHoraria
+        };
+
+        _context.Cursos.Add(curso);
+        await _context.SaveChangesAsync();
+        return curso;
+    }
+
+    public async Task<Curso> Update(int id, CursoDTO dto)
+    {
+        var curso = await _context.Cursos.FindAsync(id);
+        if (curso == null) return null;
+
+        curso.Nome = dto.Nome;
+        curso.CargaHoraria = dto.CargaHoraria;
+
+        await _context.SaveChangesAsync();
+        return curso;
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        var curso = await _context.Cursos.FindAsync(id);
+        if (curso == null) return false;
+
+        _context.Cursos.Remove(curso);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
---
+++ 2H1/2H1/Services/MatriculaService.cs
@@ -0,0 +1,26 @@
+﻿public class MatriculaService : IMatriculaService
+{
+    private readonly AppDbContext _context;
+
+    public MatriculaService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Matricula>> GetAll()
+        => await _context.Matriculas.Include(m => m.Curso).ToListAsync();
+
+    public async Task<Matricula> Add(MatriculaDTO dto)
+    {
+        var matricula = new Matricula
+        {
+            IdCurso = dto.IdCurso,
+            NomeAluno = dto.NomeAluno
+        };
+
+        _context.Matriculas.Add(matricula);
+        await _context.SaveChangesAsync();
+
+        return matricula;
+    }
+}
